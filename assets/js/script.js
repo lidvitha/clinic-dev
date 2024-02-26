0a1d011a2ebba32a4645dd62b2fad1f5334798dc
lidvitha <lidvitha29@gmail.com>
2024-02-27 01:08:30 +0530

    added maps & wa

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,3 +58,16 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+const wa = document.querySelector("[data-wa-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    wa.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    wa.classList.remove("active");
+  }
+});
