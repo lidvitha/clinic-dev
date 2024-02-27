b88fe7a1ca417869e5cdd092d0fc102c4c0d5f9f
lidvitha <lidvitha29@gmail.com>
2024-02-28 00:41:44 +0530

    added contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,14 +60,41 @@ window.addEventListener("scroll", function () {
 });
 
 
-const wa = document.querySelector("[data-wa-btn]");
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Select the elements
+//     const header = document.querySelector("header");
+//     const wa = document.querySelector("[data-wa-btn]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    wa.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    wa.classList.remove("active");
-  }
-});
+//     // Add the "active" class to both elements
+//     header.classList.add("active");
+//     wa.classList.add("active");
+// });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-4YQagI-GHLSOGmNUhTc2MMrKXNli4BajiDRiI99aWGxmn4ruvIcvohnx4cdhDr1d/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
+
+
+document.addEventListener("DOMContentLoaded", function() {
+        // Function to get current timestamp in IST
+        function getCurrentTimestampIST() {
+            var currentDate = new Date();
+            var utcOffset = 5.5 * 60; // IST is UTC+5.5
+            var istOffset = currentDate.getTimezoneOffset() + utcOffset;
+            currentDate.setMinutes(currentDate.getMinutes() + istOffset);
+            return currentDate.toISOString();
+        }
+
+        // Automatically populate the hidden input field with current timestamp in IST
+        document.querySelector('form[name="contact-form"]').addEventListener("submit", function() {
+            document.getElementById("timestamp").value = getCurrentTimestampIST();
+        });
+    });
